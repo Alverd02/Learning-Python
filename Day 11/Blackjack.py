5c17dc64734729e4ef837a0294c3aabc67151388
Albert <albert.montane02@gmail.com>
2023-02-22 02:45:24 +0100

    Blackjack

--- Day 11/Blackjack.py
+++ Day 11/Blackjack.py
@@ -19,6 +19,9 @@ def user_next_play(hand):
         next_play = input("Type \"get\" to get a card or type \"pass\" to pass: ")
         if next_play == "get":
             hand.append(random.choice(cards))
+            if sum(hand) > 21 and 11 in hand:
+                index = hand.index(11)
+                hand[index] = 1
             if sum(hand) < 21:
                 print(f"Your hand: {hand}, total score: {sum(hand)}")
                 print(f"Dealer first card: {dealer[0]}")
@@ -30,10 +33,16 @@ def dealer_next_play(hand):
     while sum(hand) < 21:
         if sum(dealer) < 17:
             dealer.append(random.choice(cards))
+            if sum(hand) > 21 and 11 in hand:
+                index = hand.index(11)
+                hand[index] = 1
         else:
             dealer_next_play = random.choice(["get", "pass"])
             if dealer_next_play == "get":
                 dealer.append(random.choice(cards))
+                if sum(hand) > 21 and 11 in hand:
+                    index = hand.index(11)
+                    hand[index] = 1
             else:
                 break
     return hand
@@ -46,8 +55,27 @@ while game == True:
         user_hand,dealer = new_game()
         dealer = dealer_next_play(dealer)
         user_hand = user_next_play(user_hand)
-        if sum(user_hand) == sum(dealer) and sum(user_hand) < 21:
-            print("It's a draw, both of you have the same core")
+        print(f"Your hand: {user_hand}, total score: {sum(user_hand)}")
+        print(f"Dealer total score: {sum(dealer)}")
+        if (sum(dealer) or sum(user_hand)) == 21:
+            if sum(dealer) == 21:
+                print("Blackjcak!!!! Dealer wins")
+            else:
+                print("Blackjcak!!!! You win")
+        elif sum(user_hand) and sum(dealer) < 21:
+            if sum(user_hand) == sum(dealer):
+                print("It's a draw, both of you have the same core")
+            elif sum(user_hand) > sum(dealer):
+                print("You win!!!")
+            else:
+                print("You lose :(")
+        elif sum(user_hand) or sum(dealer) > 21:
+            if sum(user_hand) > 21 and sum(dealer) < 21:
+                print("You went over 21, you lose.")
+            elif sum(dealer) > 21 and sum(user_hand) < 21:
+                print("The dealer went over 21, you win")
+            else:
+                print("Both of you went over ")
     else:
         game == False
 
