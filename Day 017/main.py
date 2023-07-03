b4ac7b14899d489ab0c49d917cde107b1dee8ba8
Albert <albert.montane02@gmail.com>
2023-07-04 00:40:48 +0200

    Quiz brain

---
+++ Day 017/main.py
@@ -0,0 +1,14 @@
+from question_model import question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for i in question_data:
+    q = question(i["text"], i["answer"])
+    question_bank.append(q)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+    quiz.check_answer()
+    print("\n")
