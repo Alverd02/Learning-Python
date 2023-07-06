3b39fe4cfe8076af897d8f98d48dd6b8aa4ef0db
Albert <albert.montane02@gmail.com>
2023-07-06 02:55:20 +0200

    Snake Game

--- Day 020/Snake Game.py
+++ Day 020/Snake Game.py
@@ -11,54 +11,70 @@ def first_body():
     for i in range(3):
         body = Turtle()
         body.color("white")
-        body.fillcolor("white")
         body.shape("square")
         body.penup()
         body.speed(0)
-        body.goto(20 * i, 0)
+        body.goto(-20 * i, 0)
         snake.append(body)
     return snake
 
+
 window.listen()
 snake = first_body()
-def turn_right():
+
+
+def up():
     snake[0].speed(0)
-    snake[0].right(90)
-def turn_left():
+    snake[0].setheading(90)
+
+
+def right():
     snake[0].speed(0)
-    snake[0].right(270)
+    snake[0].setheading(0)
+
+
+def left():
+    snake[0].speed(0)
+    snake[0].setheading(180)
+
+
+def down():
+    snake[0].speed(0)
+    snake[0].setheading(270)
+
+
 def move(snake):
     for j in range(len(snake)):
-        window.onkey(fun=turn_right, key="d")
-        window.onkey(fun=turn_left, key="a")
+        window.onkey(fun=right, key="d")
+        window.onkey(fun=left, key="a")
+        window.onkey(fun=up, key="w")
+        window.onkey(fun=down, key="s")
         print(snake[j].heading())
         if j == 0:
             print(snake[j].heading())
             if snake[j].heading() == 0:
                 snake[j].speed(1)
                 x, y = snake[j].position()
-                snake[j].goto(x+20, y)
+                snake[j].goto(x + 20, y)
             elif snake[j].heading() == 180:
                 snake[j].speed(1)
                 x, y = snake[j].position()
-                snake[j].goto(x-20, y)
+                snake[j].goto(x - 20, y)
             elif snake[j].heading() == 270:
                 snake[j].speed(1)
                 x, y = snake[j].position()
-                snake[j].goto(x, y-20)
+                snake[j].goto(x, y - 20)
             else:
                 snake[j].speed(1)
                 x, y = snake[j].position()
-                snake[j].goto(x, y+20)
+                snake[j].goto(x, y + 20)
         else:
             snake[j].speed(1)
-            x, y = snake[j-1].position()
+            x, y = snake[j - 1].position()
             snake[j].goto(x, y)
 
 
-
 play = True
-
 
 while play:
     move(snake)
