60e3a9c019c113192742ac5d2f1c886a4115b661
Albert <albert.montane02@gmail.com>
2023-07-06 18:57:02 +0200

    Snake Game

--- Day 020/Snake Game.py
+++ Day 020/Snake Game.py
@@ -1,26 +1,24 @@
+import time
 from turtle import Screen, Turtle
 
 window = Screen()
 window.setup(600, 600)
 window.bgcolor("black")
 window.title("Snake Game")
-
-
-def first_body():
-    snake = []
-    for i in range(3):
-        body = Turtle()
-        body.color("white")
-        body.shape("square")
-        body.penup()
-        body.speed(0)
-        body.goto(-20 * i, 0)
-        snake.append(body)
-    return snake
-
-
 window.listen()
-snake = first_body()
+window.tracer(0)
+
+snake = []
+
+for i in range(3):
+    body = Turtle()
+    body.color("white")
+    body.shape("square")
+    body.penup()
+    body.speed(0)
+    body.goto(-20 * i, 0)
+    snake.append(body)
+
 
 
 def up():
@@ -43,40 +41,38 @@ def down():
     snake[0].setheading(270)
 
 
-def move(snake):
+play = True
+while play:
+    window.update()
+    time.sleep(1)
     for j in range(len(snake)):
         window.onkey(fun=right, key="d")
         window.onkey(fun=left, key="a")
         window.onkey(fun=up, key="w")
         window.onkey(fun=down, key="s")
-        print(snake[j].heading())
         if j == 0:
-            print(snake[j].heading())
-            if snake[j].heading() == 0:
-                snake[j].speed(1)
-                x, y = snake[j].position()
-                snake[j].goto(x + 20, y)
-            elif snake[j].heading() == 180:
-                snake[j].speed(1)
-                x, y = snake[j].position()
-                snake[j].goto(x - 20, y)
-            elif snake[j].heading() == 270:
-                snake[j].speed(1)
-                x, y = snake[j].position()
-                snake[j].goto(x, y - 20)
-            else:
-                snake[j].speed(1)
-                x, y = snake[j].position()
-                snake[j].goto(x, y + 20)
+            snake[j].speed(1)
+            snake[j].forward(20)
         else:
             snake[j].speed(1)
             x, y = snake[j - 1].position()
             snake[j].goto(x, y)
 
 
-play = True
-
-while play:
-    move(snake)
-
 window.exitonclick()
+# if snake[j].heading() == 0:
+#     snake[j].speed(1)
+#     x, y = snake[j].position()
+#     snake[j].goto(x + 20, y)
+# elif snake[j].heading() == 180:
+#     snake[j].speed(1)
+#     x, y = snake[j].position()
+#     snake[j].goto(x - 20, y)
+# elif snake[j].heading() == 270:
+#     snake[j].speed(1)
+#     x, y = snake[j].position()
+#     snake[j].goto(x, y - 20)
+# else:
+#     snake[j].speed(1)
+#     x, y = snake[j].position()
+#     snake[j].goto(x, y + 20)
