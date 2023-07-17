1ac680add45cccc2a2dc3c092734652550815470
Albert <albert.montane02@gmail.com>
2023-07-18 01:27:54 +0200

    Turtle Crossing Capstone

--- Day 023/scoreboard.py
+++ Day 023/scoreboard.py
@@ -1,5 +1,12 @@
 FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
-
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        self.level_number = 1
+        super().__init__()
+        self.clear()
+        self.hideturtle()
+        self.penup()
+        self.goto(-220, 260)
+        self.write(f"Level {self.level_number}", align="center", font = FONT)
